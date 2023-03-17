282586d152fca2bd9e43ae132bb0a647ad580039
suzz-in <o_o_su_jin_@naver.com>
2023-03-17 17:51:31 +0900

    feat: 무한스크롤 기능완성

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import axios from 'axios'
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query'
 import PokemonCard from '@/components/cards/PokemonCard'
 import styles from "../styles/Home.module.css"
+import { useRef } from 'react'
+import useIntersectionObserver from '@/hook/useObeserver'
 
 
 interface Pokemon {
@@ -11,8 +13,9 @@ interface Pokemon {
 
 export default function Home() {
 
+  const endLine = useRef(null)
+
   const getAllPokemon = async ({pageParam=0}) => {
-    console.log("pageParams",pageParam)
   return await axios.get("https://pokeapi.co/api/v2/pokemon", {
     params: {
       limit: 30,
@@ -36,7 +39,6 @@ const {
   //무한스크롤의 핵심이다. getNextPageParam 메서드가 falsy한 값을 반환하면 추가 fetch 실행하지 않음
   //falsy하지 않은 값을 return 할 경우 Number를 리턴해야함 fetch calback의 인자로 자동으로 PageParam을 전달
   getNextPageParam: (lastPage) => {
-    console.log("lastPage", lastPage)
     const nextpage = lastPage.next;
     if(!nextpage) return false;
     
@@ -45,6 +47,13 @@ const {
 })
 
 
+
+useIntersectionObserver({
+  root: null,
+  target: endLine,
+  onIntersect: fetchNextPage,
+  enabled: hasNextPage,
+})
 
 
   return (
@@ -67,7 +76,7 @@ const {
       
 
     </section>
-    <button onClick={()=> fetchNextPage()} disabled={!hasNextPage || isFetchingNextPage}>다음페이지</button>
+    <div ref={endLine}/>
     </>
   )
 }
